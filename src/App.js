fd6974a0230f4f24094bfbf43efc62c209380096
Johannes Wagener <johannes@wagener.cc>
2016-04-11 12:07:51 +0200

    pick playlists

--- src/App.js
+++ src/App.js
@@ -2,62 +2,83 @@ import React, { Component } from 'react';
 var request = require('superagent');
 import _ from 'underscore';
 
+var SPOTIFY_TOKEN;
+
 export default class App extends Component {
   constructor() {
     super(...arguments);
     this.state = {};
   }
 
-  get(url, cb){
-    request.get(url).set('Authorization', 'Bearer ' + this.at ).end((err, res) => {
-      var data = JSON.parse(res.text);
-    // Calling the end function will send the request
-      cb(data);
-    });
-  }
-
   componentDidMount() {
-    this.at = window.location.hash.split(/=|&/)[1];
-    if(this.at){
-
-      request.get("https://api.spotify.com/v1/me/playlists").set('Authorization', 'Bearer ' + this.at ).end((err, res) => {
-        var data = JSON.parse(res.text);
-      // Calling the end function will send the request
+    SPOTIFY_TOKEN = window.location.hash.split(/=|&/)[1];
+    if(SPOTIFY_TOKEN){
+      get("https://api.spotify.com/v1/me/playlists", (data) => {
         this.setState({playlists: data.items});
-        console.log(data);
       });
     }
-  }
-
-  handleClick(e){
-    this.get(e.currentTarget.href, (tracks) => {
-      console.log("tracks", tracks);
-    })
-    e.preventDefault();
   }
 
   render() {
     var spotifyUrl = "https://accounts.spotify.com/authorize?client_id=8bd399434dcf455eb66fdb500adb7288&";
     spotifyUrl += "redirect_uri=" + encodeURIComponent(window.location.toString().split("#")[0]);
     spotifyUrl += "&scope=user-read-private%20user-read-email%20playlist-read-private&response_type=token&state=123";
-    //<span className="playlist-name">{playlist.name}</span>
 
     if(this.state.playlists){
       return <div className="home">
+        <p><b>Spotify - SoundCloud</b> lets you take your playlists from Spotify to SoundCloud. That's it.</p>
+        <a className="step step-1 step-done" href={spotifyUrl}>Connect to Spotify </a>
         <a className="step step-2">Connect to SoundCloud </a>
-        <a className="step step-3">Take your playlists with you.</a>
+        <a className="step step-3">Pick your playlists.</a>
         {_.map(this.state.playlists, (playlist) => {
-          return <a href={playlist.tracks.href} playlist={playlist} onClick={this.handleClick.bind(this)} className="playlist">
-            <img className="playlist-image" src={playlist.images[0].url} />
-          </a>
+          return <Playlist key={playlist.id} playlist={playlist} />
         })}
+
       </div>
     }
     return <div className="home">
+      <p><b>Spotify - SoundCloud</b> lets you take your playlists from Spotify to SoundCloud. That's it.</p>
       <a className="step step-1" href={spotifyUrl}>Connect to Spotify </a>
       <a className="step step-2">Connect to SoundCloud </a>
       <a className="step step-3">Take your playlists with you.</a>
-      {this.state.playlists && this.state.playlists.join(", ")}
     </div>
   }
+}
+
+class Playlist extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      selected: false
+    };
+  }
+
+  handleClick(e){
+    this.setState({
+      selected: !this.state.selected
+    });
+    return e.preventDefault();
+    get(e.currentTarget.href, (tracks) => {
+      console.log("tracks", tracks);
+    });
+    e.preventDefault();
+  }
+
+  render(){
+    var playlist = this.props.playlist;
+    return <div className="playlist-wrapper">
+      <a href={playlist.tracks.href} playlist={playlist} onClick={this.handleClick.bind(this)} className={`playlist ${this.state.selected ? "playlist-selected" : ""}`}>
+        <img className="playlist-image" src={playlist.images[0].url} />
+        <span className="playlist-name">{playlist.name}</span>
+
+      </a>
+    </div>
+  }
+}
+
+function get(url, cb){
+  request.get(url).set('Authorization', 'Bearer ' + SPOTIFY_TOKEN ).end((err, res) => {
+    var data = JSON.parse(res.text);
+    cb(data);
+  });
 }
