642845895ff5b615a93891a69dbc286bc9ee69c6
Johannes Wagener <johannes@wagener.cc>
2016-04-11 11:19:00 +0200

    heroku ready

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var express = require('express');
+var app = express();
+//var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require(process.env.NODE_ENV === "production" ? './webpack.config.prod' : './webpack.config.dev');
+app.use(require('webpack-dev-middleware')(webpack(webpackConfig), {noInfo: true, publicPath: webpackConfig.output.publicPath}));
+
+app.get('/', function(request, response) {
+  response.sendFile('index.html', {root: __dirname});
+});
+
 
 var port = process.env.PORT || 3000;
-
-new WebpackDevServer(webpack(config), {
+app.listen(port, function() {
+  console.log('Listening on ' + port + '...');
+});
+/*
+new WebpackDevServer(webpack(webpackConfig), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
@@ -13,4 +24,4 @@ new WebpackDevServer(webpack(config), {
     return console.log(err);
   }
   console.log('Listening on ' + port + '...');
-});
+});*/
---
+++ webpack.config.prod.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
